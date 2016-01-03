19a79d437c17a5580d98ac9b462c272c323f95ae
Katie Lee <leekf@rose-hulman.edu>
2016-01-03 18:20:40 -0500

    Fixed Interface Relations and Added a Test

--- src/problem/asm/ClassDeclarationVisitor.java
+++ src/problem/asm/ClassDeclarationVisitor.java
@@ -9,11 +9,12 @@ import problem.models.impl.Model;
 import problem.models.impl.Relation;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Opcodes;
 
 public class ClassDeclarationVisitor extends ClassVisitor implements IClazzGetter {
 	private Model model;
 	private IClass clazz;
-	
+
 	public ClassDeclarationVisitor(int api, Model m) {
 		super(api);
 		this.model = m;
@@ -22,22 +23,33 @@ public class ClassDeclarationVisitor extends ClassVisitor implements IClazzGette
 
 	@Override
 	public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-		
-		//removes everything except the last part of the class name
+
+		boolean isClass = true;
+		if ((access & Opcodes.ACC_INTERFACE) != 0) {
+			isClass = false;
+		}
+
+		// removes everything except the last part of the class name
 		String[] nameSplit = name.split("/");
-		name = nameSplit[nameSplit.length -1];
-		
+		name = nameSplit[nameSplit.length - 1];
+
 		this.clazz = new Class(name);
-		IRelation r = new Relation(superName, interfaces);
-		r.addSuperClass(name, superName);
-		r.addInterfaces(name, interfaces);
+		IRelation r;
+		if (!isClass && interfaces.length > 0) {
+			r = new Relation(interfaces[0]);
+			r.addSuperClass(name, interfaces[0]);
+		} else {			
+			r = new Relation(superName, interfaces);
+			r.addSuperClass(name, superName);
+			r.addInterfaces(name, interfaces);
+		}
 		this.clazz.addRelation(r);
 		this.model.addRelation(r);
 		this.model.addClazz(clazz);
-		
+
 		super.visit(version, access, name, signature, superName, interfaces);
 	}
-	
+
 	public IClass getClazz() {
 		return this.clazz;
 	}
--- test/problem/test/tests/GVTesting.java
+++ test/problem/test/tests/GVTesting.java
@@ -3,8 +3,11 @@ package problem.test.tests;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,82 +18,81 @@ import org.junit.Test;
 import problem.AppLauncher;
 
 public class GVTesting {
-	
+
 	private TestDesignParser parser;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		parser = new TestDesignParser();		
+		parser = new TestDesignParser();
 	}
 
 	@Test
 	public final void testEmptyClass() throws IOException {
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+		String[] classes = { "problem.test.classes.EmptyClass" };
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+		BufferedReader buffer = new BufferedReader(reader);
+
+		String file = "";
+		String line = "";
+		while ((line = buffer.readLine()) != null) {
+			file += line;
+		}
+		System.out.println(file);
+		
+		String gv = "digraph model{rankdir = BT;EmptyClass [shape=\"record\",label = \"{EmptyClass| + init() : void\\"+"l}\"];//writing relations between classes nowEmptyClass -> Object [arrowhead = \"empty\"];}";
+		System.out.println(gv);
+		assertEquals(gv, file); 
+		
+		buffer.close();
+		reader.close();
+	}
+
+	@Test
+	public final void testClassWithField() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
-	
+
 	@Test
-	public final void testClassWithField() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+	public final void testClassWithMethod() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
-	
+
 	@Test
-	public final void testClassWithMethod() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+	public final void testClassWithFieldAndMethod() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
-	
+
 	@Test
-	public final void testClassWithFieldAndMethod() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+	public final void testClassWithSuperClass() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
-	
+
 	@Test
-	public final void testClassWithSuperClass() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+	public final void testClassWithInterface() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
-	
+
 	@Test
-	public final void testClassWithInterface() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+	public final void testInterfaceWithSuperClass() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
-	
+
 	@Test
-	public final void testInterfaceWithSuperClass() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"	
-		};
-		parser.parse(classes);
-		FileReader reader = new FileReader("input_output/test.gv");
-	}
-	
-	@Test
-	public final void testClassWithEverything() throws IOException{
-		String[] classes = {
-				"problem.test.classes.EmptyClass"
-		};
+	public final void testClassWithEverything() throws IOException {
+		String[] classes = { "problem.test.classes.EmptyClass" };
 		parser.parse(classes);
 		FileReader reader = new FileReader("input_output/test.gv");
 	}
