07f2c7dee32019a4520e06a503bdd3fda25a2009
Trent Punt <tpunt3@gmail.com>
2016-01-03 17:50:45 -0500

    setup for automated testing

---
+++ test/problem/test/tests/GVTesting.java
@@ -0,0 +1,98 @@
+package problem.test.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import problem.AppLauncher;
+
+public class GVTesting {
+	
+	private TestDesignParser parser;
+	
+	@Before
+	public void setUp() throws Exception {
+		parser = new TestDesignParser();		
+	}
+
+	@Test
+	public final void testEmptyClass() throws IOException {
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testClassWithField() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testClassWithMethod() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testClassWithFieldAndMethod() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testClassWithSuperClass() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testClassWithInterface() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testInterfaceWithSuperClass() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"	
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+	
+	@Test
+	public final void testClassWithEverything() throws IOException{
+		String[] classes = {
+				"problem.test.classes.EmptyClass"
+		};
+		parser.parse(classes);
+		FileReader reader = new FileReader("input_output/test.gv");
+	}
+
+}
---
+++ test/problem/test/tests/TestDesignParser.java
@@ -0,0 +1,81 @@
+package problem.test.tests;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Opcodes;
+
+import problem.asm.ClassDeclarationVisitor;
+import problem.asm.ClassFieldVisitor;
+import problem.asm.ClassMethodVisitor;
+import problem.model.visitor.IModelTraverser;
+import problem.model.visitor.IModelVisitor;
+import problem.models.impl.Model;
+import problem.models.impl.ModelGVOutputStream;
+
+public class TestDesignParser {
+	
+	public TestDesignParser(){
+		
+	}
+
+	public void parse(String[] CLASSES) throws IOException {
+
+		Model model = new Model();
+
+		for (String className : CLASSES)
+
+		{
+			// ASM's ClassReader does the heavy lifting of parsing the compiled
+			// Java class
+			ClassReader reader = new ClassReader(className);
+
+			// make class declaration visitor to get superclass and interfaces
+			ClassVisitor decVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, model);
+
+			// DECORATE declaration visitor with field visitor
+			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, decVisitor, model);
+
+			// DECORATE field visitor with method visitor
+			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, model);
+
+			// TODO: add more DECORATORS here in later milestones to accomplish
+			// specific tasks
+			// Tell the Reader to use our (heavily decorated) ClassVisitor to
+			// visit the class
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+
+			// this is what they do in CarApp???
+			OutputStream out = new FileOutputStream("input_output/test.gv");
+			IModelVisitor gvWriter = new ModelGVOutputStream(out);
+			IModelTraverser traverser = (IModelTraverser) model;
+			traverser.accept(gvWriter);
+			out.close();
+
+			// runs DOT on our .gv file. This might need to be moved?
+			// ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
+			// "\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\release\\bin\\dot\"
+			// -Tpng input_output/model.gv > input_output/graph1.png");
+			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
+					"\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\" -Tpng input_output/model.gv > input_output/graph1.png");
+			builder.redirectErrorStream(true);
+			Process p = builder.start();
+			String line;
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while (true) {
+				line = r.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+		}
+
+		System.out.println(model.getClasses().toString());
+	}
+}
