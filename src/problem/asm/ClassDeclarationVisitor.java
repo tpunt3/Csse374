990932c87b5de71be5d06b366bed6467aa4517ce
Katie Lee <leekf@rose-hulman.edu>
2016-01-03 17:18:06 -0500

    Checking Parsing
    Checking to make sure the java parsing works

--- src/problem/asm/ClassDeclarationVisitor.java
+++ src/problem/asm/ClassDeclarationVisitor.java
@@ -23,9 +23,6 @@ public class ClassDeclarationVisitor extends ClassVisitor implements IClazzGette
 	@Override
 	public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
 		
-		// TODO: construct an internal representation of the class for later use
-		// by decorators
-		
 		//removes everything except the last part of the class name
 		String[] nameSplit = name.split("/");
 		name = nameSplit[nameSplit.length -1];
