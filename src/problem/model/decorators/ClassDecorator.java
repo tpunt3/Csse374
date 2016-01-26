c7bad004eff85d09a91419480eb15edcde9585ea
Katie Lee <leekf@rose-hulman.edu>
2016-01-25 23:45:48 -0500

    Decorator Classes
    Began creating decorator framework

---
+++ src/problem/model/decorators/ClassDecorator.java
@@ -0,0 +1,78 @@
+package problem.model.decorators;
+
+import java.util.Collection;
+
+import problem.model.visitor.IVisitor;
+import problem.models.api.IClass;
+import problem.models.api.IField;
+import problem.models.api.IMethod;
+
+public abstract class ClassDecorator implements IClass {
+
+	@Override
+	public void accept(IVisitor v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<IMethod> getMethods() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<IField> getFields() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addMethod(IMethod m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addField(IField f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getIsClass() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setHasStaticField(boolean hasStaticField) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHasPrivateConstructor(boolean hasPrivateConstructor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHasPublicStaticMethod(boolean hasPublicStaticMethod) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isSingleton() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
---
+++ src/problem/model/decorators/SingletonDecorator.java
@@ -0,0 +1,9 @@
+package problem.model.decorators;
+
+public class SingletonDecorator extends ClassDecorator {
+
+	public SingletonDecorator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
