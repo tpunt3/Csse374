205a4abe94e9e6d26e96be61c8b54074fd6f04f3
Trent Punt <tpunt3@gmail.com>
2016-01-28 14:29:26 -0500

    fixed singleton detector to detect extension of singletons

--- src/problem/model/decorators/SingletonDetector.java
+++ src/problem/model/decorators/SingletonDetector.java
@@ -19,25 +19,20 @@ public class SingletonDetector implements IPatternDetector {
 	@Override
 	public void detectPatterns(){
 		for(IClass c : model.getClasses()){
-			boolean privateConstructor = false;
-			boolean returnsSelf = false;
-			boolean isEnum = false;
 			
-			if(c.getSignature() != null && c.getSignature().contains("Enum")){
-				isEnum = true;
-			}
+			boolean extendsSingleton = false;
 			
-			for(IMethod m : c.getMethods()){
-				if((m.getAccessNumber() == 9 || m.getAccessNumber() == 41) && m.getReturnType().contains(m.getClazz().getName())){
-					returnsSelf = true;
-				}
-				
-				if((m.getAccess().equals("-") || m.getAccess().equals("#")) && m.getName().equals("init")){
-					privateConstructor = true;
+			
+			
+			for(IClass c2 : model.getClasses()){
+				if(c2.getName().equals(c.getSuperClass())){
+					if(checkForSingleton(c2)){
+						extendsSingleton = true;
+					}
 				}
 			}
 			
-			if(privateConstructor && returnsSelf && !isEnum){
+			if(checkForSingleton(c) || extendsSingleton){
 				this.toReplace.add(c);
 			}
 		}
@@ -45,5 +40,30 @@ public class SingletonDetector implements IPatternDetector {
 		for (IClass c : this.toReplace){
 			this.model.replaceClass(c, new SingletonDecorator(c));
 		}
+	}
+	
+	private boolean checkForSingleton(IClass c){
+		boolean privateConstructor = false;
+		boolean returnsSelf = false;
+		boolean isEnum = false;
+		
+		if(c.getSignature() != null && c.getSignature().contains("Enum")){
+			isEnum = true;
+		}
+		
+		for(IMethod m : c.getMethods()){
+			if((m.getAccessNumber() == 9 || m.getAccessNumber() == 41) && m.getReturnType().contains(m.getClazz().getName())){
+				returnsSelf = true;
+			}
+			
+			if((m.getAccess().equals("-") || m.getAccess().equals("#")) && m.getName().equals("init")){
+				privateConstructor = true;
+			}
+		}
+		
+		if(privateConstructor && returnsSelf && !isEnum){
+			return true;
+		}
+		return false;
 	}
 }
