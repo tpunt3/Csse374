5d2da58b0bc8820512c125228e531ad41f474f51
Katie Lee <leekf@rose-hulman.edu>
2016-02-09 17:29:57 -0500

    SD Test work again!

--- test/problem/test/tests/BasicSDTesting.java
+++ test/problem/test/tests/BasicSDTesting.java
@@ -42,7 +42,8 @@ public class BasicSDTesting {
 	
 	@Test
 	public void testMediumDepth() throws IOException, InterruptedException {
-		DesignParser parser= new DesignParser("\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\"","\"C:\\Users\\punttj\\Desktop\\csse374\\finalProject\\sdedit-4.2-beta1.exe\"");
+		//DesignParser parser= new DesignParser("\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\"","\"C:\\Users\\punttj\\Desktop\\csse374\\finalProject\\sdedit-4.2-beta1.exe\"");
+		DesignParser parser = new DesignParser("\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\release\\bin\\dot\"", "\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\sdedit-4.2-beta1.exe\"");
 		String[] classes = {"problem.sdTest.classes.A"};
 		parser.generateDocuments(DocType.sd, "problem.sdTest.classes.A,A,a1", 4, classes);
 		
@@ -72,7 +73,8 @@ public class BasicSDTesting {
 	
 	@Test 
 	public void testBigDepth() throws IOException {
-		DesignParser parser= new DesignParser("\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\"","\"C:\\Users\\punttj\\Desktop\\csse374\\finalProject\\sdedit-4.2-beta1.exe\"");
+		//DesignParser parser= new DesignParser("\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\"","\"C:\\Users\\punttj\\Desktop\\csse374\\finalProject\\sdedit-4.2-beta1.exe\"");
+		DesignParser parser = new DesignParser("\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\release\\bin\\dot\"", "\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\sdedit-4.2-beta1.exe\"");
 		String[] classes = {"problem.sdTest.classes.A"};
 		parser.generateDocuments(DocType.sd, "problem.sdTest.classes.A,A,a1", 10, classes);
 		
--- test/problem/test/tests/ProjectSDTesting.java
+++ test/problem/test/tests/ProjectSDTesting.java
@@ -15,35 +15,49 @@ public class ProjectSDTesting {
 
 	@Test
 	public void testClassAccept() throws IOException {
-		//DesignParser parser= new DesignParser("\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\"","\"C:\\Users\\punttj\\Desktop\\csse374\\finalProject\\sdedit-4.2-beta1.exe\"");
-		DesignParser parser = new DesignParser("\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\release\\bin\\dot\"", "\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\sdedit-4.2-beta1.exe\"");
-		String[] classes = {"problem.models.impl.Class"};
+		// DesignParser parser= new
+		// DesignParser("\"C:\\Users\\punttj\\Desktop\\csse374\\release\\bin\\dot\"","\"C:\\Users\\punttj\\Desktop\\csse374\\finalProject\\sdedit-4.2-beta1.exe\"");
+		DesignParser parser = new DesignParser("\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\release\\bin\\dot\"",
+				"\"C:\\Users\\leekf\\Documents\\JUNIOR\\CSSE374\\sdedit-4.2-beta1.exe\"");
+		String[] classes = { "problem.models.impl.Class" };
 		parser.generateDocuments(DocType.sd, "problem.models.impl.Class,Class,accept,IVisitor", 5, classes);
-		
+
 		FileReader reader = new FileReader("input_output/sequence.sd");
 		BufferedReader buffer = new BufferedReader(reader);
-		
+
 		String file = "";
 		String line = "";
 
 		while ((line = buffer.readLine()) != null) {
 			file += line;
 		}
-		
+
 		assertTrue(file.contains("Class:Class[a]"));
 		assertTrue(file.contains("IVisitor:IVisitor[a]"));
 		assertTrue(file.contains("IField:IField[a]"));
 		assertTrue(file.contains("IMethod:IMethod[a]"));
 		assertTrue(file.contains("Collection:Collection[a]"));
-		
-		assertTrue(file.contains("Class:void=IVisitor.preVisit(IClass)"));
-		assertTrue(file.contains("Class:void=IVisitor.visit(IClass)"));
-		assertTrue(file.contains("Class:void=IField.accept(IModelVisitor)"));
+		assertTrue(file.contains("HashSet:HashSet[a]"));
+		assertTrue(file.contains("Iterator:Iterator[a]"));
+		assertTrue(file.contains("Set:Set[a]"));
+		assertTrue(file.contains("HashMap:HashMap[a]"));
+
+		assertTrue(file.contains("Class:void=IVisitor.preVisit(ITraverser)"));
+		assertTrue(file.contains("Class:void=IVisitor.visit(ITraverser)"));
+		assertTrue(file.contains("Class:Iterator=Collection.iterator()"));
+		assertTrue(file.contains("Class:Object=Iterator.next()"));
+		assertTrue(file.contains("Class:void=IField.accept(IVisitor)"));
+		assertTrue(file.contains("Class:boolean=Iterator.hasNext()"));
 		assertTrue(file.contains("Class:boolean=Collection.isEmpty()"));
-		assertTrue(file.contains("Class:void=IVisitor.intermediateVisit(IClass)"));
-		assertTrue(file.contains("Class:void=IMethod.accept(IModelVisitor)"));
-		assertTrue(file.contains("Class:void=IVisitor.postVisit(IClass)"));
-		
+		assertTrue(file.contains("Class:void=IVisitor.intermediateVisit(ITraverser)"));
+		assertTrue(file.contains("Class:Iterator=HashSet.iterator()"));
+		assertTrue(file.contains("HashSet:Set=HashMap.keySet()"));
+		assertTrue(file.contains("HashSet:Iterator=Set.iterator()"));
+		assertTrue(file.contains("Class:Object=Iterator.next()"));
+		assertTrue(file.contains("Class:void=IMethod.accept(IVisitor)"));
+		assertTrue(file.contains("Class:boolean=Iterator.hasNext()"));
+		assertTrue(file.contains("Class:void=IVisitor.postVisit(ITraverser)"));
+
 	}
 
 }
